41e532c35d090f1c66e47849203d460c4b93769f
AdrianD94 <ady.drozman2007@gmail.com>
2019-06-16 18:51:59 +0300

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
 
 const App =()=> {
  
@@ -20,17 +20,12 @@ const App =()=> {
         <div className='App'>
           <Navbar />
           <div className='container'>
-            <Alert alert={alert} />
+            <Alert />
             <Switch>
               <Route
                 exact
                 path='/'
-                render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
+                component={Home}
               />
               <Route exact path='/about' component={About} />
               <Route
@@ -38,6 +33,7 @@ const App =()=> {
                 path='/user/:login'
                 component={User}
               />
+              <Route component={NotFound}/>
             </Switch>
           </div>
         </div>
